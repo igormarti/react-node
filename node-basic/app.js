1bede7e8d536784dac37ee7e063207535c11cef7
igorrocha <igorrocha@msigroup.com.br>
2019-07-02 14:47:48 -0300

    finalize first block

--- node-basic/app.js
+++ node-basic/app.js
@@ -13,12 +13,18 @@ const fs = require('fs');
 
 //fs.watch('test.txt', (response) => console.log(response))
 
+
+const handleErr = (err) => console.log(err);
+const handleData = (data) => console.log(data.toString());
+
+const data = fs.readFileSync('test.txt');
+console.log(data.toString())
+console.log('Node JS programing synchronous...')
+
 fs.readFile('test.txt',(err,data)=>{
-    if(err){
-        console.logI(err)
-    }else{
-        console.log(data.toString())
-    }
+    if(err) handleErr(err);
+
+    handleData(data);
 })
 
 console.log('Node  JS programing asynchronous...')
