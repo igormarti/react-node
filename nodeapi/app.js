9ca80984acf1ebd014edb896799a3b7a8f2e2971
Igor Martins <igorm573@gmail.com>
2019-08-22 23:41:42 -0300

    Implemented connection with db mongoose

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -1,19 +1,30 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
 const morgan = require('morgan')
 const fs = require("fs")
 const path = require('path')
+const dotenv = require('dotenv')
 
+//config dotenv
+dotenv.config()
+//connecting db mogoose
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true})
+.then(()=>{ console.log('DB connected succefully') })
+//verify if has error in connect with db
+mongoose.connection.on('error',err=>{
+    console.log(`DB connection error ${err}`)
+})
 //Brings route
 const  getPosts  = require('./route/post')
-
+//Save all requests 
 app.use(morgan("common", {
     stream: fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'})
 }))
+//Middleware of the post routers 
 app.use("/",getPosts)
-
-
-app.listen(2222, () => {
-    console.log("Serve is standing...")
+//Get port
+const port = process.env.PORT || 2222
+app.listen(port, () => {
+    console.log("Serve is standing, port:"+port)
 })
