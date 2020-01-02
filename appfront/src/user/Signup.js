99ca05353485e78dfdf74e10ffc2f8253bb23aa1
igorrocha <igorrocha@msigroup.com.br>
2020-01-02 14:02:06 -0300

    Refactoring Sign up code.

--- appfront/src/user/Signup.js
+++ appfront/src/user/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Alert from '../alert/Alert'
 
 export default class Signup extends Component {
 
@@ -9,12 +10,13 @@ export default class Signup extends Component {
             name:"",
             email:"",
             password:"",
-            error:""
+            error:"",
+            success:false
         }
     }
 
     handleChange = field => e =>{
-        this.setState({[field]:e.target.value})
+        this.setState({[field]:e.target.value,error:""})
     }
 
     submitForm = e => {
@@ -25,12 +27,14 @@ export default class Signup extends Component {
         const user = {name,email,password}
         //Executing request http to back-end
         this.signUp(user).then(data => {
-            if(data.error)   this.setState({error:data.error})
-                else this.setState({
+            if(data.error)  this.setState({error:data.error,success:false})
+                
+            else this.setState({
                         name:'',
                         email:'',
                         password:'',
-                        error:''
+                        error:'',
+                        success:true,
                     })
             
         })
@@ -39,53 +43,62 @@ export default class Signup extends Component {
     signUp = (user) => {
 
        return  fetch(
-            'http://127.0.0.1:2523/signup',
-            {
-                'method':'POST',
-                'headers':{
-                    'Accept':'application/json',
-                    'Content-type':'application/json'
-                },
-                'body':JSON.stringify(user)
-            }
-        ).then((res)=>{
-            return res.json()
-        })
+               'http://127.0.0.1:2523/signup',
+               {
+                    'method':'POST',
+                    'headers':{
+                        'Accept':'application/json',
+                        'Content-type':'application/json'
+                    },
+                    'body':JSON.stringify(user)
+                }
+                ).then((res)=>{
+                    return res.json()
+                })
 
     }
+
+    signUpForm = (name,email,password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted" >Name</label>
+                <input type="text" value={name} onChange={this.handleChange('name')} className="form-control" ></input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted" >Email</label>
+                <input type="email" value={email} onChange={this.handleChange('email')} className="form-control" ></input>
+            </div>
+            <div className="form-group">
+                <label className="text-muted" >Password</label>
+                <input type="password" value={password} onChange={this.handleChange('password')} className="form-control" ></input>
+            </div>
+            <button  onClick={this.submitForm} className="btn btn-raised btn-primary btn-lg offset-5" >Submit</button>
+        </form>
+    )
     
     render() {
 
-        const {name,email,password,error} = this.state;
+        const {name,email,password,error,success} = this.state;
 
         return (
             <div className="container col-lg-12" >
+
                 <div className="col-12" >
                      <h2 className="mt-5 mb-5 justify-content-center align-items-center d-flex" >Signup</h2>
                 </div>
 
-                <div style={{display:error?'':'none'}}  className="alert alert-primary col-lg-6 col-md-12 col-sm-12 offset-lg-3 justify-content-center align-items-center"  >
-                    {error}
-                </div>
+                {
+                 error?(<Alert message={error} type="danger" />):''
+                }
+
+                {
+                 success?(<Alert message=" Account was created with success, please sign in." type="success" />):''
+                }
 
                 <div className="col-lg-6 col-md-12 col-sm-12 offset-lg-3 justify-content-center align-items-center">
-                    <form>
-                        <div className="form-group">
-                            <label className="text-muted" >Name</label>
-                            <input type="text" value={name} onChange={this.handleChange('name')} className="form-control" ></input>
-                        </div>
-                        <div className="form-group">
-                            <label className="text-muted" >Email</label>
-                            <input type="email" value={email} onChange={this.handleChange('email')} className="form-control" ></input>
-                        </div>
-                        <div className="form-group">
-                            <label className="text-muted" >Password</label>
-                            <input type="password" value={password} onChange={this.handleChange('password')} className="form-control" ></input>
-                        </div>
-
-                        <button  onClick={this.submitForm} className="btn btn-raised btn-primary btn-lg offset-5" >Submit</button>
-                    </form>
+                    {this.signUpForm(name,email,password)}
                 </div>
+
             </div>
         )
     }
