0bd5962807dc7f9cb76993e58475de2b9da858e7
igorrocha <igorrocha@msigroup.com.br>
2020-01-06 14:21:26 -0300

    Implemented signout

--- appfront/src/core/Menu.js
+++ appfront/src/core/Menu.js
@@ -10,12 +10,29 @@ const isActive = (history,path) => {
     }
 }
 
+export const signOut = (next) =>{
+    if(typeof window !== 'undefined') localStorage.removeItem('jwt')
+    next()
+    return fetch('http://127.0.0.1:2523/signout',{
+        'method':'GET'
+    }).then(res => {
+        return res.json()
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+
 const Menu = ({history}) => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-primary">
             <Link className="navbar-brand" to="/" style={{color:isActive(history,'/')}} >Home</Link>
             <Link className="navbar-brand" to="/signup" style={{color:isActive(history,'/signup')}}  >Sign Up</Link>
             <Link className="navbar-brand" to="/signin" style={{color:isActive(history,'/signin')}} >Sign In</Link>
+            <a  className="navbar-brand" 
+                onClick={()=> signOut(()=>history.push('/signin'))} 
+                style={{color:isActive(history,'/signout')}} >
+                Sign Out
+            </a>
         </nav>
     </div>
 )
