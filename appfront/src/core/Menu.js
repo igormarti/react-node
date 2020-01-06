e35913b2f7f37d1e808a7ecc66d935313a04a6f0
igorrocha <igorrocha@msigroup.com.br>
2020-01-06 08:35:01 -0300

    Created Menu

---
+++ appfront/src/core/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link,withRouter} from 'react-router-dom'
+
+
+const isActive = (history,path) => {
+    if(history.location.pathname===path){
+        return '#ff9900'
+    }else{
+        return '#fff'
+    }
+}
+
+const Menu = ({history}) => (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+            <Link className="navbar-brand" to="/" style={{color:isActive(history,'/')}} >Home</Link>
+            <Link className="navbar-brand" to="/signup" style={{color:isActive(history,'/signup')}}  >Sign Up</Link>
+            <Link className="navbar-brand" to="/signin" style={{color:isActive(history,'/signin')}} >Sign In</Link>
+        </nav>
+    </div>
+)
+
+
+export default withRouter(Menu)
