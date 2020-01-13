b4751982dd923b9898f7279004554f19cb368318
igorrocha <igorrocha@msigroup.com.br>
2020-01-13 08:37:23 -0300

    styling card user

--- appfront/src/user/Users.js
+++ appfront/src/user/Users.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import {getUsers} from '../services/user_service'
+import defaultUserPhoto from '../images/defaultUser.jpg'
 
 class Users extends Component {
 
@@ -22,6 +23,30 @@ class Users extends Component {
         })
     }
 
+    showUsers = users => (
+        <div className="row" >
+            {
+                users.map((user,i)=>
+                
+                    (
+                        <div className="card col-md-4 " key={i}>
+                        <img className="card-img-top" src={defaultUserPhoto} 
+                            style={{height:'100%',width:'15vw',objectFit:'cover'}}
+                            alt={`${user.name} photo's.`} />
+
+                            <div className="card-body">
+                                <h5 className="card-title">{user.name}</h5>
+                                <p className="card-text">{user.email}</p>
+                                <a href="#" className="btn btn-primary">View Profile</a>
+                            </div>
+
+                        </div>
+                    )
+                )
+            }
+        </div>
+    )
+
     render() {
         const {users} = this.state
 
@@ -29,15 +54,10 @@ class Users extends Component {
             <div className="container col-lg-12" >
                 <div className="col-12" >
                      <h2 className="mt-5 mb-5 ml-5" >Users</h2>
-                        <div className="card" >
-                            {
-                                users.map((user,i)=>(
-                                    <div key={i} >
-                                           <p>{user.name}</p> 
-                                    </div>    
-                                ))
-                            }  
-                        </div>
+                   
+                        {
+                           this.showUsers(users)
+                        }  
 
                 </div>
             </div>
