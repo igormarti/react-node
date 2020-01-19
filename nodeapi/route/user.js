f57aadcc3cf24aa49605a4534b76a1db8a182e5a
Igor Martins <igorm573@gmail.com>
2020-01-19 00:56:23 -0300

    Destructuring validation user object

--- nodeapi/route/user.js
+++ nodeapi/route/user.js
@@ -11,11 +11,11 @@ const {
     } = require('../controller/user')
 const Auth = require('../middleware/auth')
 const router = express.Router()
-const validator = require('../validator/user')
+const {createSingInValidation,createSingUpValidation} = require('../validator/user')
 
 //User routers
-router.post("/signup",validator.createSingUpValidation,signup)
-router.post("/signin",validator.createSingInValidation,signin)
+router.post("/signup",createSingUpValidation,signup)
+router.post("/signin",createSingInValidation,signin)
 router.get("/signout",signout)
 router.get("/users",allUsers)
 router.get('/user/:userId',Auth.HasPermission ,getUser)
